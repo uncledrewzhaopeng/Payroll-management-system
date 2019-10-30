2ec1e4f53d370979a6ac93ac0f617debeecde3b1
CUndH <875021528@qq.com>
2019-10-30 10:42:37 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-aside width="200px">侧边栏</el-aside>
+      <el-container>
+        <el-header>头部</el-header>
+        <el-main>
+          <!-- 所有页面都在main里展示，共用侧边栏和头部 -->
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
+
+<style lang="less">
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  overflow-y: auto;
+  line-height: 1200px; // 测试用，之后删除
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 1160px; // 测试用，之后删除
+  overflow-y: auto;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+</style>
